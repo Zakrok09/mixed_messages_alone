29c1b1c6a62c40d84cd2fbb5ce83ef8e7f64c844
Zakrok09 <zakrokliga@gmail.com>
2021-03-19 17:30:21 +0200

    Expand possible message components

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 //Possible words/phrases stored in arrays.
-const greetings = ["Hello there,", "Howdy,", "Seems like", "Woah,", "Hewwo,", "Hola,", "Yo"];
-const looking = ["pretty fabilous", "astonishing", "dirty", "like LinuxTechTips", "sleepy", "exhausted"];
-const question = ["Wanna hit the gym?", "Wanna go to the cinema?", "Wanna go eat some enchiladas?", "Wanna play LoL?", "Wanna watch YouTube?", "Wanna invest in Dogecoin?"];
+const greetings = ["Hello there,", "Howdy,", "Seems like", "Woah,", "Hewwo,", "Hola,", "Yo", "Waddapp", "Wazzap"];
+const looking = ["pretty fabilous", "astonishing", "dirty", "like LinuxTechTips", "sleepy", "exhausted", "like someone who codes a lot (cool)", "happy"];
+const question = ["Wanna hit the gym?", "Wanna go to the cinema?", "Wanna go eat some enchiladas?", "Wanna play LoL?", "Wanna watch YouTube?", "Wanna invest in Dogecoin?", "Wanna go skydiving?"];
 
 //Generating three random numbers to use when forming the message.
 let rand1 = Math.floor(Math.random() * greetings.length);
